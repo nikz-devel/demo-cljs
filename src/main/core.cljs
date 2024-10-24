2c85fcefe62a67d40fee71f456cfa676dba5bb56
Nikolay Zaikin <n.zaikin@reg.ru>
2024-10-24 23:02:48 +0500

    Автообновление страницы

--- src/main/core.cljs
+++ src/main/core.cljs
@@ -1,19 +1,22 @@
 (ns core
-  (:require [goog.dom :as gdom]
-            [reagent.dom :as dom]
+  (:require [reagent.dom :as dom]
             ["sweetalert2$default" :as Swal]))
 
 (defn simple-component []
   [:div
-   [:p "Hello from component!"]
+   [:p "Hello from super component!"]
    [:button {:on-click #(.fire Swal (clj->js {:title "It`s work!"
                                               :text "emmmmaaaa"
                                               :icon "success"
                                               :confirmButtonTest "Cool"}))} "Click me"]])
 
-(defn init! []
-  (dom/render [simple-component] (gdom/getElement "app")))
+(defn app []
+  [simple-component])
 
-(comment
-  (js/alert "Hello")
-  )
+(defn ^:dev/after-load start []
+  (dom/render [app]
+              (.getElementById js/document "app")))
+
+(defn init []
+  (start))
+
