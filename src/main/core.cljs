2c9112faaff0576c3c87e7e9509e047906f416dd
Nikolay Zaikin <n.zaikin@reg.ru>
2024-10-24 23:39:24 +0500

    Добавить tailwindcss

--- src/main/core.cljs
+++ src/main/core.cljs
@@ -5,10 +5,16 @@
 (defn simple-component []
   [:div
    [:p "Hello from super component!"]
-   [:button {:on-click #(.fire Swal (clj->js {:title "It`s work!"
-                                              :text "emmmmaaaa"
-                                              :icon "success"
-                                              :confirmButtonTest "Cool"}))} "Click me"]])
+   [:button.text-white.bg-blue-500.font-medium.rounded.text-sm.px-3.py-2.mr-2.mb-2
+    {:type "button"
+     :class ["hover:bg-blue-600"
+             "focus:ring-4"
+             "focus:ring-blue-300"]
+     :on-click #(.fire Swal (clj->js {:title "It`s work!"
+                                      :text "emmmmaaaa"
+                                      :icon "success"
+                                      :confirmButtonText "Cool"}))}
+    "Default"]])
 
 (defn app []
   [simple-component])
